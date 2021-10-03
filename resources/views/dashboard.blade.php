df4c608c2f02dae8e8bd75ba43579b3b648f09b6
ASUS <nicolausrio22@gmail.com>
2021-10-03 22:04:29 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang-"en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!--Bootstrap CDN-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap615.0.2/dist/css/bootstrap.min.css"
+        rel="styleshet" integrity="sha384-EVSTQN3/azprG1Anm3Q0gOLIm9Nao0Yz1ztcOTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        <title>Praktikum 6</title>
+
+</head>
+<body>  
+    <div class="container">   
+    @yield('content')
+</div>  
+</body>  
+</html>         
---
+++ resources/views/facultyCRUD/show.blade.php
@@ -0,0 +1,29 @@
+@extends('dashboard')
+
+	eusection('content')
+
+	<div class="d-flex justify-content-between mt-5 mb-5">
+	<div>
+	<h2>Show Faculty</h2>
+	</div>
+	<div>
+	<a class="btn btn-secondary" href="{{ route('faculties.index') }}"> Back</a>
+	</div>
+	</div>
+
+
+	<div class="row">
+	<div class="col-xs-12 col-sm-12 col-and-12">
+	<div class="form-group">
+	<strong>Faculty ID:</strong>
+	{{ $faculties->id
+	</div>
+	</div>
+	<div class="col-xs-12 col-sm-12 col-and-12">
+		<div class="form-group">
+	<strong>Faculty Name:</strong>
+	{{ $faculties->nama_fakultas }}
+    </div>
+    </div>
+	</div>
+@endsection
